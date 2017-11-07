9f6bf7c4e7deb0b45f147f22e9a6262861be73d2
Ryan Strong <ryanstrong2@yahoo.com>
2017-11-07 11:32:29 -0600

    date JavaScript improvements

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+
+var date = new Date();
+document.body.innerHTML = "<h1> Today is: " + date + "</h1>";
+//"<h2> " + (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h2>";
+// single line comment
+/*multi line
+comment*/
